e1421bdb2c7d4b76336e7d81d5ff7b8203914b31
ueharakaede <yapaari.kaede.kakoii@gmail.com>
2021-09-14 15:21:35 +0000

    [start]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,9 +5,12 @@ class TodolistsController < ApplicationController
   end
 
   def create
-  list = List.new(list_params)
-  list.save
-  redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -17,11 +20,21 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  
 private
 
   def list_params
-  params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f700373b18119a7eb4465ddc6938f387d920120770e1fa4a7f31836928ef0378b7f02f9afb6734ba752ab817d5cfc2163dd3ad346cc1a27c15d5884714fb8867'
